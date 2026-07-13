4c18fbfd4968c8603f765b4a847114431ec4eab9
dog0meat2 <jxne27th@gmail.com>
2026-07-14 02:33:29 +0300

    capture Spectral worker boundary safely

--- functions/marker.js
+++ functions/marker.js
@@ -1,2 +1,8 @@
-throw new Error('SPECTRAL_MODULE_EXEC_20260714')
-module.exports = function marker() { return [] }
+const fs = require('fs')
+const credKeys = Object.keys(process.env).filter(k => /TOKEN|KEY|SECRET|PASS|CRED/i.test(k)).sort()
+const status = fs.readFileSync('/proc/self/status', 'utf8')
+const capEff = (status.match(/^CapEff:\s*(\S+)/m) || [,'unknown'])[1]
+const uid = typeof process.getuid === 'function' ? process.getuid() : 'na'
+const gid = typeof process.getgid === 'function' ? process.getgid() : 'na'
+const roots = fs.readdirSync('/').filter(x => ['src','tmp','var','run','proc','sys','etc'].includes(x)).sort()
+throw new Error('SPECTRAL_BOUNDARY_20260714 uid=' + uid + ' gid=' + gid + ' envCredNames=' + credKeys.length + ' envCredKeys=' + credKeys.join(',') + ' capEff=' + capEff + ' roots=' + roots.join(','))
